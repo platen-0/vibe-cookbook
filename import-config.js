a33f3f7db47d8e48ae1057ffe46c424a504bfb78
platen-0 <platen-0-bight@icloud.com>
2026-07-23 23:28:00 +0300

    Connect production recipe importer

--- import-config.js
+++ import-config.js
@@ -1,5 +1,3 @@
 window.COOKBOOK_CONFIG = Object.freeze({
-  // Set this after the Worker is deployed, for example:
-  // https://vibe-cookbook-importer.<your-workers-subdomain>.workers.dev
-  importerUrl: ''
+  importerUrl: 'https://vibe-cookbook-importer.platen-0-bight.workers.dev'
 });
